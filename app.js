155169bc17ad8f1e1763de37df72aedb485829ac
allibrdoine <allibrodine81@gmail.com>
2022-07-20 17:51:09 -0500

    update template

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const { truncate } = require('fs/promises');
 const inquirer = require('inquirer');
 
-//const fs = require('fs');
-//const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 //const pageHTML = generatePage(name, github);
 
@@ -149,7 +148,13 @@ promptUser()
     .then(promptProject)
     //.then(projectAnswers => console.log(projectAnswers));
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+
+            console.log('Page created! Checkout index.html in this directory to see it!');
+        });
     });
 
 
